2ff49b42f1b409562e7bf9deefa533e1cd225cc1
Abhishek20091997 <53189546+Abhishek20091997@users.noreply.github.com>
2019-07-22 23:28:31 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class HelloWorld {
+	
+		  public String dispMessage()
+		  {
+		      return  "Hello World.";
+		      
+		  }
+		   
+		  public static void main(String s[])
+		  {
+		      HelloWorld obj=new HelloWorld();
+		     System.out.print( obj.dispMessage());
+		  }
+
+}
