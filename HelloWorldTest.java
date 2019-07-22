0ee030c4e5af6160bbc3ddad3efa950ec8802dd4
Abhishek20091997 <53189546+Abhishek20091997@users.noreply.github.com>
2019-07-22 23:29:01 +0530

    Create HelloWorldTest.java

---
+++ HelloWorldTest.java
@@ -0,0 +1,17 @@
+package day1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test() {
+		HelloWorld hello = new HelloWorld();
+		String actual = hello.dispMessage();
+		String expected ="Hello World.";
+		assertEquals(actual,expected);
+	}
+
+}
