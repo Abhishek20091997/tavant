ded73bedd3d8d8d2a65ec6c0485b0a903ade5421
Abhishek20091997 <53189546+Abhishek20091997@users.noreply.github.com>
2019-07-22 23:21:31 +0530

    Create SumofNumbersTest.java

---
+++ SumofNumbersTest.java
@@ -0,0 +1,22 @@
+package day2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SumofNumbersTest {
+
+	@Test
+	void test() {
+		SumofNumbers SoN=new SumofNumbers();
+        int actual= SoN.Sum0fEven(20, 45);
+        int expect=416;
+		assertEquals(actual,expect);
+	}
+	void test1() {
+		SumofNumbers SoN=new SumofNumbers();
+        int actual= SoN.Sum0fOdd(20, 45);
+        int expect=429;
+		assertEquals(actual,expect);
+	}
+}
