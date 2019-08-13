7cc7c4d65797644a4ce14a47222e902f158efe13
Abhishek20091997 <53189546+Abhishek20091997@users.noreply.github.com>
2019-08-13 16:03:23 +0530

    Update FactorialTest.java

--- FactorialTest.java
+++ FactorialTest.java
@@ -7,19 +7,19 @@ import org.junit.jupiter.api.Test;
 class FactorialTest {
 
 	@Test
-	void test() {
+	void testFactorialof3() {
 		Factorial f=new Factorial();
         int a = f.calculateFact(3);
         int b = 6;
         assertEquals(a,b);
 	}
-	void test1() {
+	void testFactorialof4() {
 		Factorial f=new Factorial();
         int a = f.calculateFact(4);
         int b = 24;
         assertEquals(a,b);
 	}
-	void test2() {
+	void testFactorialof5() {
 		Factorial f=new Factorial();
         int a = f.calculateFact(5);
         int b = 120;
